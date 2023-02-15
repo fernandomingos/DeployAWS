48f133d3c6934d8d6b4cc2d67eb141d5238b552c
fernandomingos <fernandomingos@gmail.com>
2023-02-15 15:08:38 -0300

    [Add] - Iniciando a adição de logs.

--- DeployAWS.API/Controllers/AuthenticationController.cs
+++ DeployAWS.API/Controllers/AuthenticationController.cs
@@ -3,6 +3,7 @@ using DeployAWS.Application.Interfaces;
 using DeployAWS.Domain.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Threading.Tasks;
 
@@ -10,16 +11,20 @@ namespace DeployAWS.API.Controllers
 {
     [Route("[controller]")]
     [ApiController]
+    [Produces("application/json")]
     public class AuthenticationController : ControllerBase
     {
 
         private readonly IApplicationServiceCustomer _applicationServiceCustomer;
         private readonly IConfiguration _configuration;
+        private readonly ILogger<AuthenticationController> _logger;
 
-        public AuthenticationController(IApplicationServiceCustomer applicationServiceCustomer, IConfiguration configuration)
+        public AuthenticationController(IApplicationServiceCustomer applicationServiceCustomer, IConfiguration configuration
+            , ILogger<AuthenticationController> logger)
         {
             _applicationServiceCustomer = applicationServiceCustomer;
             _configuration = configuration;
+            _logger = logger;
         }
 
         [HttpGet]
@@ -27,11 +32,14 @@ namespace DeployAWS.API.Controllers
         {
             try
             {
+                _logger.LogInformation($"##### Executando requisição Authentication => AuthenticationController #####");
                 var customer = await _applicationServiceCustomer.GetByIdAsync(authenticationDto.Id);
 
                 if (customer == null)
+                {
+                    _logger.LogInformation($"##### Usuário inválido! #####");
                     return BadRequest(new { Message = "Usuário inválido." });
-
+                }
 
                 var token = ServiceJwtAuth.GenerateToken(customer, _configuration);
 
@@ -42,8 +50,9 @@ namespace DeployAWS.API.Controllers
                 });
 
             }
-            catch (Exception)
+            catch (Exception ex)
             {
+                _logger.LogCritical($"##### Ocorreu um erro durante o processamento da solicitação. Detalhes: {ex.Message} #####");
                 return BadRequest(new { Message = "Ocorreu algum erro interno na aplicação, por favor tente novamente." });
             }
         }
